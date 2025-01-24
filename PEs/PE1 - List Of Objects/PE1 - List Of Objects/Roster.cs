4667116013bf7c397b9db9cca9291a66a3f3cb03
caeics <caeics@rit.edu>
2025-01-24 11:35:14 -0500

    Created the save function for demo in 1/24 class

--- PEs/PE1 - List Of Objects/PE1 - List Of Objects/Roster.cs
+++ PEs/PE1 - List Of Objects/PE1 - List Of Objects/Roster.cs
@@ -10,11 +10,47 @@ namespace PE1___List_Of_Objects
     {
         private string rosterName;
         private List<Student> students;
+        private string filename;
 
         public Roster(string rosterName)
         {
             this.rosterName = rosterName;
             this.students = new List<Student>();
+
+            filename = "../../../" + rosterName + ".txt";
+
+            //Check for the file and see if it exists
+
+            //If the file exists, load in the data and populate the students list
+
+            //If the file does not exist, no more work needs to be completed within the constructor
+        }
+
+        public void Save()
+        {
+            //Create streamwrite variable
+            StreamWriter writer = null;
+
+            try
+            {
+                writer = new StreamWriter(filename);
+
+                //Write out information for each student in the students list
+                foreach(Student s in students)
+                {
+                    writer.WriteLine($"{s.Name},{s.Major},{s.Year}");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //Close the file if it is open
+            if(writer != null)
+            {
+                writer.Close();
+            }
         }
 
         public Student AddStudent()
