e35c0c9b49f926b24426d6cd7e376ba99f7e424a
caeics <caeics@rit.edu>
2025-02-05 10:29:16 -0500

    Added the simple get and set

---
+++ Demos/ListDemo/ListDemo/ListDemo/MyStringList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class MyStringList
+    {
+        // The data for the list and a constant for the initial size
+        private String[] data;
+        private const int InitCapacity = 2;
+
+        // auto-property where the get is public, but not the set
+
+        // Create the indexer for the MyStringList Class
+
+        public String this[int i]
+        {
+            get { return data[i]; }
+            set { data[i] = value; }
+        }
+
+        public int Count { get; private set; }
+
+        // Creates a basic list
+        public MyStringList()
+        {
+            data = new String[InitCapacity];
+            Count = 0;
+        }
+
+        // Adds an item to the list
+        public void Add(String item)
+        {
+            // If we're out of space, make a bigger array,
+            // copy the data over, then make our data field refer
+            // to the new, bigger array
+            if (Count == data.Length)
+            {
+                string[] newData = new string[data.Length * 2];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    newData[i] = data[i];
+                }
+                data = newData;
+            }
+
+            // Add the new element & increment the count
+            data[Count] = item;
+            Count++;
+        }
+    }
+
+}
---
+++ Demos/ListDemo/ListDemo/ListDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStringList names = new MyStringList();
+            names.Add("Shiro");
+            names.Add("Pax");
+            names.Add("Lacey");
+
+            Console.WriteLine("Element 2 of the MyStringList " + names[1]);
+            names[2] = "Snoopy";
+            Console.WriteLine("Element 3 of the MyStringList " + names[2]);
+            Console.WriteLine("Element -1 of the MyStringList " + names[10]);
+
+        }
+    }
+}
