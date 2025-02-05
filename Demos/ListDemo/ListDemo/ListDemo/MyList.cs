f5cf2c1b4702d15b70ba54aad00b99eb6b4ddf7b
caeics <caeics@rit.edu>
2025-02-05 11:06:11 -0500

    Implemented MyList with Generics

---
+++ Demos/ListDemo/ListDemo/ListDemo/MyList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class MyList<TData>
+    {
+        // The data for the list and a constant for the initial size
+        private TData[] data;
+        private const int InitCapacity = 2;
+
+        // auto-property where the get is public, but not the set
+
+        // Create the indexer for the MyStringList Class
+
+        // Gets or sets the data at index i
+        public TData this[int i]
+        {
+            get
+            {
+                // Ensure the index is valid
+                if (i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException(
+                      "Your get index is bad");
+                }
+                return data[i];
+            }
+
+            set
+            {
+                // Ensure the index is valid
+                if (i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException(
+                      "Your set index is bad");
+                }
+                data[i] = value;
+            }
+        }
+
+
+        public int Count { get; private set; }
+
+        // Creates a basic list
+        public MyList()
+        {
+            data = new TData[InitCapacity];
+            Count = 0;
+        }
+
+        // Adds an item to the list
+        public void Add(TData item)
+        {
+            // If we're out of space, make a bigger array,
+            // copy the data over, then make our data field refer
+            // to the new, bigger array
+            if (Count == data.Length)
+            {
+                TData[] newData = new TData[data.Length * 2];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    newData[i] = data[i];
+                }
+                data = newData;
+            }
+
+            // Add the new element & increment the count
+            data[Count] = item;
+            Count++;
+        }
+    }
+}
--- Demos/ListDemo/ListDemo/ListDemo/Program.cs
+++ Demos/ListDemo/ListDemo/ListDemo/Program.cs
@@ -14,12 +14,34 @@
             Console.WriteLine("Element 3 of the MyStringList " + names[2]);
             try
             {
-                Console.WriteLine("Element -1 of the MyStringList " + names[10]);
+ //               Console.WriteLine("Element -1 of the MyStringList " + names[10]);
             }
             catch (IndexOutOfRangeException e)
             {
                 Console.WriteLine(e.Message);
             }
+
+            // Try using the Generic version called MyList
+
+            MyList<string> list = new MyList<string>();
+            list.Add("Bob");
+            list.Add("Mary");
+            list.Add("Paul");
+
+            Console.WriteLine("The value of our string generic list is " + list[1] + " at position 2");
+
+            MyList<int> myIntList = new MyList<int>();
+            myIntList.Add(1);
+            myIntList.Add(42);
+            myIntList.Add(3);
+
+            Console.WriteLine("The value of our int generic list is " + myIntList[1] + " at position 2");
+
+            Dictionary<int,string> myDictionary = new Dictionary<int,string>();
+            MyList<Dictionary<int,string>> myDictionaryList = new MyList<Dictionary<int,string>>();
+
+            myDictionaryList.Add(myDictionary);
+
         }
     }
 }
